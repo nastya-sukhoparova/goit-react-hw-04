59657b9612cc4384e03b6be4dc8d2e7694dc61c4
Nastya Sukhoparova <nastyadalchenko1997@gmail.com>
2024-11-18 12:15:04 +0200

    react

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,60 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ImageCard from "../ImageCard/ImageCard";
-import "./ImageGallery.module.css";
+import styles from "./ImageGallery.module.css";
 
-const ImageGallery = ({ query }) => {
-  const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    if (!query) return;
-
-    const fetchImages = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch(
-          `https://api.unsplash.com/search/photos?query=${query}&page=${page}&per_page=12&client_id=ScM033pCkgdwz-G7adw5ruAAR3qQkEE2lAhV32MiUso`
-        );
-        const data = await response.json();
-        console.log("Fetched data:", data);
-
-        if (data.results.length === 0) {
-          console.log("No images found for query:", query);
-        }
-
-        setImages((prevImages) => [...prevImages, ...data.results]);
-        setTotalPages(Math.ceil(data.total / 12));
-      } catch (error) {
-        console.error("Error fetching images:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchImages();
-  }, [query, page]);
-
-  const handleLoadMore = () => {
-    setPage((prevPage) => prevPage + 1);
-  };
-
+const ImageGallery = ({ images, onImageClick }) => {
   return (
-    <div>
-      <ul>
-        {images.map((image) => (
-          <li key={image.id}>
-            <ImageCard image={image} />
-          </li>
-        ))}
-      </ul>
-
-      {isLoading && <p>Loading...</p>}
-      {page < totalPages && !isLoading && (
-        <button onClick={handleLoadMore}>Load more...</button>
-      )}
-    </div>
+    <ul className={styles.gallery}>
+      {images.map((image) => (
+        <li key={image.id} className={styles.galleryItem}>
+          <ImageCard image={image} onClick={onImageClick} />
+        </li>
+      ))}
+    </ul>
   );
 };
 
